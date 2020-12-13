d2d89a0d69401dbcffa67916eb7ac8335e678c5c
Hui Yu <ityuhui@gmail.com>
2020-12-13 17:53:15 +0800

    reorg

---
+++ raft-client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+var (
+	help bool
+
+	I     string
+	peers string
+)
+
+func init() {
+	flag.BoolVar(&help, "help", false, "Show the usage")
+	flag.StringVar(&I, "I", "", "My name:port")
+	flag.StringVar(&peers, "Peers", "", "The name:port of my peers")
+
+	flag.Usage = usage
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `
+Usage:
+raft-go --I=myip:port --Peers=host2:port,host3:port
+`)
+	flag.PrintDefaults()
+}
+
+func main() {
+
+	flag.Parse()
+	if help || I == "" || peers == "" {
+		flag.Usage()
+		return
+	}
+
+}
