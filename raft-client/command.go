e4b798bdc15747faa5b3e736c3198018f25a1e4f
Hui Yu <ityuhui@gmail.com>
2021-03-14 14:19:21 +0800

    fix golint

--- raft-client/command.go
+++ raft-client/command.go
@@ -2,11 +2,13 @@ package main
 
 import "raft-go/common"
 
+//Command : data structure of command
 type Command struct {
 	Mode common.CommandMode
 	Text string
 }
 
+//ParseCommand : parse string to command
 func ParseCommand(get string, set string) *Command {
 	cmd := &Command{
 		Mode: common.COMMANDMODE_UNKNOWN,
@@ -22,6 +24,7 @@ func ParseCommand(get string, set string) *Command {
 	return cmd
 }
 
+//ToString : convert command mode to string
 func (cmd *Command) ToString() string {
 	return cmd.Mode.ToString() + " " + cmd.Text
 }
