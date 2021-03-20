ff2ffea7a2b2e8904a7cac7b0dc8440d8d1ea858
Hui Yu <ityuhui@gmail.com>
2021-03-20 19:29:21 +0800

    golint

--- common/address.go
+++ common/address.go
@@ -5,11 +5,13 @@ import (
 	"strings"
 )
 
+//Address : the address of a node
 type Address struct {
 	Name string
 	Port string
 }
 
+//ParseAddress : parase the string to address data structure
 func ParseAddress(addrString string) *Address {
 	splited := strings.Split(addrString, ":")
 	if 2 == len(splited) && splited[0] != "" && splited[1] != "" {
@@ -17,12 +19,12 @@ func ParseAddress(addrString string) *Address {
 			Name: splited[0],
 			Port: splited[1],
 		}
-	} else {
-		log.Fatalf("The address string is invalid.")
-		return nil
 	}
+	log.Fatalf("The address string is invalid.")
+	return nil
 }
 
+//GenerateUName : generate an unique name for a node
 func (addr *Address) GenerateUName() string {
 	return addr.Name + ":" + addr.Port
 }
